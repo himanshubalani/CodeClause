b23ca40ca42bbc9f2bfa46dc5cb44ec4d57525d5
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2022-12-15 12:31:49 +0530

    Added Files

---
+++ Calculator/main.py
@@ -0,0 +1,67 @@
+import pyfiglet
+import operations as op
+
+pyfiglet.print_figlet("CodeClause Calciulator", font="doom")
+
+print("Choose an operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Power")
+print("6.Square root")
+print("7.Sine")
+print("8.Cosine")
+print("9.Tangent")
+
+choice = input("Enter choice(1/2/3/4/5/6/7/8/9):")
+
+def inpnum1():
+	return int(input("Enter first number: "))
+
+def inpnum2():
+	return int(input("Enter second number: "))
+
+if choice == '1':
+	num1 = inpnum1()
+	num2 = inpnum2()
+	print(num1,"+",num2,"=", op.add(num1,num2))
+
+elif choice == '2':
+	num1 = inpnum1()
+	num2 = inpnum2()
+	print(num1,"-",num2,"=", op.subtract(num1,num2))
+
+elif choice == '3':
+	num1 = inpnum1()
+	num2 = inpnum2()
+	print(num1,"*",num2,"=", op.multiply(num1,num2))
+
+elif choice == '4':
+	num1 = inpnum1()
+	num2 = inpnum2()
+	print(num1,"/",num2,"=", op.divide(num1,num2))
+
+elif choice == '5':
+	num1 = inpnum1()
+	num2 = inpnum2()
+	print(num1,"^",num2,"=", op.power(num1,num2))
+
+elif choice == '6':
+	num1 = inpnum1()
+	print("Square root of",num1,"=", op.square_root(num1))
+
+elif choice == '7':
+	num1 = inpnum1()
+	print("Sine of",num1,"=", op.sine(num1))
+
+elif choice == '8':
+	num1 = inpnum1()
+	print("Cosine of",num1,"=", op.cosine(num1))
+
+elif choice == '9':
+	num1 = inpnum1()
+	print("Tangent of",num1,"=", op.tangent(num1))
+
+else:
+	print("Invalid input")
---
+++ Calculator/operations.py
@@ -0,0 +1,33 @@
+# Path: Calculator\operations.py
+
+import math
+
+def	add(num1, num2):
+	return num1 + num2
+
+def	subtract(num1, num2):
+
+	return num1 - num2
+
+def	multiply(num1, num2):
+	return num1 * num2
+
+def	divide(num1, num2):
+	return num1 / num2
+
+def	power(num1, num2):
+	return num1 ** num2
+
+def	square_root(num1):
+	return num1 ** 0.5
+
+def	sine(num1):
+	return math.sin(num1)
+
+def	cosine(num1):
+	return math.cos(num1)
+
+def	tangent(num1):
+	return math.tan(num1)
+
+# Path: Calculator\main.py
